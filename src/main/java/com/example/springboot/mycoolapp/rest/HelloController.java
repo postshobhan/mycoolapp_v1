1a20af2faac27669eb9bf39421ec6309e046f905
shobhan <post.shobhan@gmail.com>
2022-04-09 14:18:25 -0400

    Changed doc

--- src/main/java/com/example/springboot/mycoolapp/rest/HelloController.java
+++ src/main/java/com/example/springboot/mycoolapp/rest/HelloController.java
@@ -8,23 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	
+
 	@Value("${team.name}")
 	private String teamName;
-	
+
 	@Value("${team.email}")
 	private String teamEmail;
 
 	@GetMapping("/")
 	public String sayHello() {
-		return "Hello ! The time is " + LocalDateTime.now() + " Team name is: " + teamName + " & Team email is: " + teamEmail;
+		return "Hello user! The time is " + LocalDateTime.now() + " Team name is: " + teamName + " & Team email is: "
+				+ teamEmail;
 	}
-	
+
 	@GetMapping("/app1")
 	public String app1() {
 		return "Hello from app1";
 	}
-	
+
 	@GetMapping("/app2")
 	public String app2() {
 		return "Hello from app2";
